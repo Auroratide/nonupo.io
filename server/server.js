6b866157fa44e1ced49ded8086cd3eca2eccb063
Timothy Foster <tf.auroratide@gmail.com>
2021-04-28 12:44:36 -0500

    Restructured into a yarn-based monorepo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 app.get('*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+   res.sendFile(path.resolve(__dirname, '..', 'client', 'public', 'index.html'));
 })
 
 app.listen(port, () => {
