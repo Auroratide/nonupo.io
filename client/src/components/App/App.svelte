f367923fdad7258a084b1d23d0444de394fe19b3
Timothy Foster <tf.auroratide@gmail.com>
2021-05-01 16:14:24 -0500

    Remove starter kit styling

--- client/src/components/App/App.svelte
+++ client/src/components/App/App.svelte
@@ -9,18 +9,3 @@
 <main>
 	<Nonupo />
 </main>
-
-<style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
