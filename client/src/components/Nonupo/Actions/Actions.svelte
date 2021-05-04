1057f4e68dc3bb6e1ef2f77addf47fe0b812af09
Timothy Foster <tf.auroratide@gmail.com>
2021-05-04 16:38:07 -0500

    Basic styling around action options

--- client/src/components/Nonupo/Actions/Actions.svelte
+++ client/src/components/Nonupo/Actions/Actions.svelte
@@ -7,10 +7,30 @@
 
 <fieldset class="options">
     <legend>Grid Options</legend>
-    <label for="option-number">{step.num}</label>
     <input type="radio" id="option-number" name="options" bind:group={currentSelection} value="n" />
-    <label for="option-plus">+</label>
+    <label for="option-number">{step.num}</label>
     <input type="radio" id="option-plus" name="options" bind:group={currentSelection} value="+" />
-    <label for="option-minus">-</label>
+    <label for="option-plus">+</label>
     <input type="radio" id="option-minus" name="options" bind:group={currentSelection} value="-" />
+    <label for="option-minus">-</label>
 </fieldset>
+
+<style lang="scss">
+    .options {
+        border: none;
+        text-align: center;
+        font-size: 3em;
+
+        legend {
+            display: none;
+        }
+
+        input:checked + label {
+            color: red;
+        }
+
+        input {
+            display: none;
+        }
+    }
+</style>
