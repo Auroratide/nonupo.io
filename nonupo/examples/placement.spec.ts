1da7c1a295dfda9821f520897a79bc0c0d8c64f3
Timothy Foster <tf.auroratide@gmail.com>
2021-05-05 09:06:31 -0500

    Group illegal move tests together

--- nonupo/examples/placement.spec.ts
+++ nonupo/examples/placement.spec.ts
@@ -18,26 +18,28 @@ describe('placement', () => {
         expect(nextStep.grid[4]).toEqual(Nonupo.Operator.Plus)
     })
 
-    test('illegal to override a filled square', () => {
-        const step = new Nonupo.PlaceStep(buildGrid({ 1: Nonupo.Number.One }), Nonupo.Number.Five, new Nonupo.D10())
-
-        expect(() => step.placeNumber(1)).toThrow(Nonupo.Exception)
-        expect(step.grid[1]).toEqual(Nonupo.Number.One)
-    })
-
-    test('illegal to place operators adjacent to one another', () => {
-        const step = new Nonupo.PlaceStep(buildGrid({ 7: Nonupo.Operator.Plus }), Nonupo.Number.Five, new Nonupo.D10())
-
-        expect(() => step.placeOperator(1, Nonupo.Operator.Plus)).toThrow(Nonupo.Exception)
-        expect(step.grid[1]).toEqual(Nonupo.Empty)
-
-        expect(() => step.placeOperator(6, Nonupo.Operator.Minus)).toThrow(Nonupo.Exception)
-        expect(step.grid[6]).toEqual(Nonupo.Empty)
-
-        expect(() => step.placeOperator(8, Nonupo.Operator.Plus)).toThrow(Nonupo.Exception)
-        expect(step.grid[8]).toEqual(Nonupo.Empty)
-
-        expect(() => step.placeOperator(13, Nonupo.Operator.Minus)).toThrow(Nonupo.Exception)
-        expect(step.grid[13]).toEqual(Nonupo.Empty)
+    describe('illegal moves', () => {
+        test('overriding a filled square', () => {
+            const step = new Nonupo.PlaceStep(buildGrid({ 1: Nonupo.Number.One }), Nonupo.Number.Five, new Nonupo.D10())
+    
+            expect(() => step.placeNumber(1)).toThrow(Nonupo.Exception)
+            expect(step.grid[1]).toEqual(Nonupo.Number.One)
+        })
+    
+        test('placing operators adjacent to one another', () => {
+            const step = new Nonupo.PlaceStep(buildGrid({ 7: Nonupo.Operator.Plus }), Nonupo.Number.Five, new Nonupo.D10())
+    
+            expect(() => step.placeOperator(1, Nonupo.Operator.Plus)).toThrow(Nonupo.Exception)
+            expect(step.grid[1]).toEqual(Nonupo.Empty)
+    
+            expect(() => step.placeOperator(6, Nonupo.Operator.Minus)).toThrow(Nonupo.Exception)
+            expect(step.grid[6]).toEqual(Nonupo.Empty)
+    
+            expect(() => step.placeOperator(8, Nonupo.Operator.Plus)).toThrow(Nonupo.Exception)
+            expect(step.grid[8]).toEqual(Nonupo.Empty)
+    
+            expect(() => step.placeOperator(13, Nonupo.Operator.Minus)).toThrow(Nonupo.Exception)
+            expect(step.grid[13]).toEqual(Nonupo.Empty)
+        })
     })
 })
