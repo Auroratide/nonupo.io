d41d82943956ab1a66609ab3569fc6eaea4eaf62
Timothy Foster <tf.auroratide@gmail.com>
2021-05-06 16:54:20 -0500

    Start making pencil circle around the options

--- client/src/components/Nonupo/Actions/Actions.svelte
+++ client/src/components/Nonupo/Actions/Actions.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import * as Nonupo from '@auroratide/nonupo'
+    import PencilOption from './PencilOption.svelte'
 
     export let step: Nonupo.PlaceStep
     export let currentSelection: string
@@ -7,12 +8,18 @@
 
 <fieldset class="options">
     <legend>Grid Options</legend>
-    <input type="radio" id="option-number" name="options" bind:group={currentSelection} value={step.num} />
-    <label for="option-number">{step.num}</label>
-    <input type="radio" id="option-plus" name="options" bind:group={currentSelection} value={Nonupo.Grid.Operator.Plus} />
-    <label for="option-plus">+</label>
-    <input type="radio" id="option-minus" name="options" bind:group={currentSelection} value={Nonupo.Grid.Operator.Minus} />
-    <label for="option-minus">-</label>
+    <PencilOption>
+        <input type="radio" id="option-number" name="options" bind:group={currentSelection} value={step.num} />
+        <label for="option-number">{step.num}</label>
+    </PencilOption>
+    <PencilOption>
+        <input type="radio" id="option-plus" name="options" bind:group={currentSelection} value={Nonupo.Grid.Operator.Plus} />
+        <label for="option-plus">+</label>
+    </PencilOption>
+    <PencilOption>
+        <input type="radio" id="option-minus" name="options" bind:group={currentSelection} value={Nonupo.Grid.Operator.Minus} />
+        <label for="option-minus">-</label>
+    </PencilOption>
 </fieldset>
 
 <style lang="scss">
@@ -31,6 +38,14 @@
 
         input {
             display: none;
+        }
+
+        label {
+            display: inline-block;
+            width: 1em;
+            height: 1em;
+            border-radius: 1em;
+            cursor: pointer;
         }
     }
 </style>
---
+++ client/src/components/Nonupo/Actions/PencilOption.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    const p = 20   // padding
+    const w = 1200 // width
+</script>
+
+<div class="container">
+    <slot></slot>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="-{w / 2 + p} -{w / 2 + p} {w + 2 * p} {w + 2 * p}">
+        <circle cx="0" cy="0" r="{w / 2}" />
+    </svg>
+</div>
+
+<style lang="scss">
+    .container {
+        display: inline-block;
+        position: relative;
+        width: 1em;
+        height: 1em;
+        line-height: 1;
+    }
+
+    svg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        pointer-events: none;
+    }
+
+    circle {
+        fill: none;
+        stroke: var(--skin-color-fg);
+        stroke-width: 60;
+    }
+</style>
